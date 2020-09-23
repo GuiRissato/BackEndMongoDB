b768947e3ddb746bf76e6efb01595c6aea207fb2
GuiRissato <52382877+GuiRissato@users.noreply.github.com>
2020-09-23 16:36:47 -0300

    conexao com banco de dados

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+
+
+
+
+
+
+// criando um bloco assincrono e ja executando esse bloco
+(async () => {
+
+
+//  se for uma aplcacao real e um banco que voce quer proteger a senha, nao coloque essas informacoes no GitHub
+// procure sempre utilizar as variaveis de ambiente para isso
+const connectionString = 'mongodb+srv://admin:yt132tzRNjeyHaA2@cluster0.gvmnm.mongodb.net/Ocean_mogo_DB?retryWrites=true&w=majority';
+
+// async/await
+console.info('Conectando o banco de dados MongoDB')
+// variavel que vai fazer a conexao com o banco de dados
+const client = await mongodb.MongoClient.connect(connectionString,{
+    useUnifiedTopology: true
+
+});
+console.info('Banco de dados conectado com sucesso')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -41,3 +64,5 @@ app.delete('/mensagens/:id', (req, res) => {
 app.listen(port, () => {
     console.log(`App rodando em http://localhost:${port}`);
 });
+// fecho o bloco assincrono e executo
+})();
